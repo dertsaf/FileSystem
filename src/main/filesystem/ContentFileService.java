c077ad7763040d562d78d45fc902d11a5d8dee22
Max Dertsaf <der.max7@gmail.com>
2023-09-12 17:04:50 +0200

    Add service documentation

--- src/main/filesystem/ContentFileService.java
+++ src/main/filesystem/ContentFileService.java
@@ -3,8 +3,28 @@ package filesystem;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The ContentFileService provides methods for working with files based on their content.
+ * It allows you to copy content from one file to another and check if two files have the same content.
+ */
 public interface ContentFileService {
+
+    /**
+     * Copies the content of one file to another.
+     *
+     * @param destinationFile The file where the content will be copied to.
+     * @param fileToCopy      The file whose content will be copied.
+     * @throws IOException If an I/O error occurs during the copy operation.
+     */
     void copyContent(File destinationFile, File fileToCopy) throws IOException;
 
+    /**
+     * Checks whether two files have the same content.
+     *
+     * @param expected The first file for content comparison.
+     * @param actual   The second file for content comparison.
+     * @return true if the files have the same content, false otherwise.
+     * @throws IOException If an I/O error occurs during the comparison.
+     */
     boolean haveSameContent(File expected, File actual) throws IOException;
 }
--- src/main/filesystem/FileSystemService.java
+++ src/main/filesystem/FileSystemService.java
@@ -2,24 +2,44 @@ package filesystem;
 
 import java.io.IOException;
 
+/**
+ * The FileSystemService provides methods for managing files in a file system.
+ * These methods allow you to add, update, delete, and read files in the file system.
+ */
 public interface FileSystemService {
+
     /**
-     * todo
+     * Adds a file to the file system at the specified destination path.
+     *
+     * @param destinationFile The path where the file should be added in the file system.
+     * @param fileToAdd       The path to the file to be added to the file system.
+     * @throws IOException If an I/O error occurs during the operation.
      */
     void addFile(String destinationFile, String fileToAdd) throws IOException;
 
     /**
-     * todo
+     * Updates the contents of a file in the file system with the contents of another file.
+     *
+     * @param destinationFile The path of the file to be updated in the file system.
+     * @param fileToUpdateWith The path to the file whose contents will be used for the update.
+     * @throws IOException If an I/O error occurs during the operation.
      */
     void updateFile(String destinationFile, String fileToUpdateWith) throws IOException;
 
     /**
-     * todo
+     * Deletes a file from the file system.
+     *
+     * @param fileToDelete The path of the file to be deleted from the file system.
+     * @throws IOException If an I/O error occurs during the operation.
      */
     void deleteFile(String fileToDelete) throws IOException;
 
     /**
-     * todo
+     * Reads the contents of a file from the file system and copies it to another file.
+     *
+     * @param fileToReadFrom The path of the file to be read from the file system.
+     * @param fileToCopyTo   The path to the file where the contents will be copied.
+     * @throws IOException If an I/O error occurs during the operation.
      */
     void readFile(String fileToReadFrom, String fileToCopyTo) throws IOException;
 }
